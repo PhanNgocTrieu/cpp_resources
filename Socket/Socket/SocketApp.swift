5c4dadc03364d3396b82bc1456080bb85269acce
trieupn <pntrieu200799@gmail.com>
2023-10-06 00:37:02 +0700

    update

---
+++ Socket/Socket/SocketApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocketApp.swift
+//  Socket
+//
+//  Created by tpn on 27/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct SocketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
