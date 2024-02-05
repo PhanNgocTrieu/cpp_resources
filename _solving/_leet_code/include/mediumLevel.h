60069764edb3c303c9e377b9e655c30897d4c1bd
phantrieu <9930343-Z100@jp-global.aisingroup.com>
2024-02-05 23:45:19 +0700

    Update files for serve God

--- _solving/_leet_code/include/mediumLevel.h
+++ _solving/_leet_code/include/mediumLevel.h
@@ -510,6 +510,20 @@ namespace leetcode
          * @return std::string the result after involked the command as the path
          */
         std::string simplifyPath(std::string path);
+
+        /*
+            Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent, 
+            with the colors in the order red, white, and blue.
+
+            We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
+
+            You must solve this problem without using the library's sort function.
+
+            Example:
+                Input: nums = [2,0,2,1,1,0]
+                Output: [0,0,1,1,2,2]
+        */
+        void sortColors(vector<int>& nums);
         
     private:
         /* ===================== These functions for supporting ============================= */
--- _solving/_leet_code/src/mediumLevel.cpp
+++ _solving/_leet_code/src/mediumLevel.cpp
@@ -1290,4 +1290,20 @@ namespace leetcode
 
         return res;
     }
+
+    void mediumLevel::sortColors(vector<int>& nums) {
+        int low = 0, high = nums.size() - 1, i = 0;
+        while (i <= high) {
+            if (nums[i] == 0) {
+                std::swap(nums[i], nums[low]);
+                ++low;
+                ++i;
+            } else if (nums[i] == 2) {
+                std::swap(nums[i], nums[high]);
+                --high;
+            } else {
+                ++i;
+            }
+        }
+    }
 }
